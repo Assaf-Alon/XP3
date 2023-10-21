2fdbdb468a712a5f6e4eecb80e21f9079ecbeeaa
Assaf-Alon <67502145+Assaf-Alon@users.noreply.github.com>
2023-10-21 21:13:20 +0300

    Create python-tests.yaml

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """Definitions of variables used for configurations, such as directory paths"""
 from decouple import config
 
-EMAIL_ADDRESS = str(config("EMAIL_ADDRESS", default="your-mail@mail.com", cast=str))
+EMAIL_ADDRESS = str(config("EMAIL_ADDRESS", default="alon.assaf@campus.technion.ac.il", cast=str))
 TEST_DOWNLOAD_PATH = str(config("TEST_DOWNLOAD_PATH", default="C:\\Temp\\DOMinion.png", cast=str))
 IS_DEBUG = config("DEBUG", default=False)
 
@@ -10,4 +10,8 @@ MP3_DIR = str(config("MP3_DIR", cast=str))
 MP4_DIR = str(config("MP4_DIR", cast=str))
 IMG_DIR = str(config("IMG_DIR", cast=str))
 TMP_DIR = str(config("TMP_DIR", cast=str))
-DEFAULT_PLAYLIST = str(config("DEFAULT_PLAYLIST", cast=str))
+DEFAULT_PLAYLIST = str(
+    config(
+        "DEFAULT_PLAYLIST", cast=str, default="https://www.youtube.com/playlist?list=PLofmCZWRdOtl1dM2XQPx2_8KxveP6KbTt"
+    )
+)
